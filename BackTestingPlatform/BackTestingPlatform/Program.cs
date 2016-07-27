788aa665578db9586717af28f5c211ae83933c77
0FlynnHe0 <hfy89757@qq.com>
2016-07-27 13:02:15 +0800

    test072701

--- BackTestingPlatform/BackTestingPlatform/Program.cs
+++ BackTestingPlatform/BackTestingPlatform/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BackTestingPlatform.Strategies.MA_Strategy;
 
 namespace BackTestingPlatform
 {
@@ -10,6 +11,7 @@ namespace BackTestingPlatform
     {
         static void Main(string[] args)
         {
+            MATest mytest = new MATest();               
 
         }
     }
---
+++ BackTestingPlatform/BackTestingPlatform/Strategies/MA_Strategy/MAtest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackTestingPlatform.Strategies.MA_Strategy
+{
+    class MATest
+    {
+       public void MainStrategy()
+       {
+           GetData myData = new GetData();
+           myData.Execute();   
+       }
+    }
+}
