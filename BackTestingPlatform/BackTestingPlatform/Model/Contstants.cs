abe92136fab176388eeb422681179535bd45a5eb
heng mao <184772181@qq.com>
2016-08-05 14:45:30 +0800

    添加填充部分

--- BackTestingPlatform/BackTestingPlatform/Model/Contstants.cs
+++ BackTestingPlatform/BackTestingPlatform/Model/Contstants.cs
@@ -12,7 +12,6 @@ namespace BackTestingPlatform.Model
     public class Constants
     {
         public static int INF = 99999, NINF = -99999, NONE = -88888;
-
         public static DateTime TRADE_DAY_START = new DateTime(2007, 1, 1, 0, 0, 0);
         public static DateTime TRADE_DAY_END = new DateTime(2016, 12, 31, 23, 59, 59);
     }
