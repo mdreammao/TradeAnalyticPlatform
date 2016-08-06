bd09bfdfd55b1549dd12b46d33e757d68d3ff639
heng mao <mdream05301@163.com>
2016-08-06 21:19:04 +0800

    更新

---
+++ BackTestingPlatform/BackTestingPlatform/Utilities/Option/OptionUtilities.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BackTestingPlatform.Model.Option;
+
+namespace BackTestingPlatform.Utilities.Option
+{
+    public static class OptionUtilities
+    {
+
+        public static List<OptionInfo> getOptionListByOptionType(List<OptionInfo> list, string type)
+        {
+            return list.FindAll(delegate (OptionInfo item)
+            {
+                if (item.optionType == type)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            );
+        }
+        public static List<OptionInfo> getOptionListByStrike(List<OptionInfo> list, double strikeLower,double strikeUpper)
+        {
+            return list.FindAll(delegate (OptionInfo item)
+            {
+                if (item.strike >= strikeLower && item.strike<=strikeUpper)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            );
+        }
+
+        public static List<OptionInfo> getOptionListByStrike(List<OptionInfo> list,double strike )
+        {
+            return list.FindAll(delegate (OptionInfo item)
+            {
+                if (item.strike==strike)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            );
+        }
+        public static List<OptionInfo> getOptionListByDate(List<OptionInfo> list, int firstDay,int lastDay)
+        {
+            return list.FindAll(delegate (OptionInfo item)
+            {
+                if (Convert.ToInt32(item.startDate.ToString("yyyyMMdd"))<=lastDay && Convert.ToInt32(item.endDate.ToString("yyyyMMdd")) >= firstDay)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            );
+        }
+
+        public static List<OptionInfo> getOptionListByDate(List<OptionInfo> list, int date)
+        {
+            return list.FindAll(delegate (OptionInfo item)
+            {
+                if (Convert.ToInt32(item.startDate.ToString("yyyyMMdd")) <= date && Convert.ToInt32(item.endDate.ToString("yyyyMMdd")) >= date)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            );
+        }
+
+        //public static List<OptionInfo> getOptionListByDate(List<OptionInfo> list,int date)
+        //{
+        //    List<OptionInfo> listToday = new List<OptionInfo>();
+        //    foreach (var item in list)
+        //    {
+        //        int start = Convert.ToInt32(item.startDate.ToString("yyyyMMdd"));
+        //        int end = Convert.ToInt32(item.endDate.ToString("yyyyMMdd"));
+        //        if (date>=start && end>=date)
+        //        {
+        //            listToday.Add(item);
+        //        }
+        //    }
+        //    return listToday;
+        //}
+    }
+
+   
+}
