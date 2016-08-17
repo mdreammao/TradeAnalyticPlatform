a098be137a7f0e7362a0da8cda562dce5f246ef1
hzk <zk.huang@qq.com>
2016-08-17 09:32:03 +0800

    Kit中DateTime相关函数改进

--- BackTestingPlatform/BackTestingPlatformTest/Tests/ToolKits.cs
+++ BackTestingPlatform/BackTestingPlatformTest/Tests/ToolKits.cs
@@ -16,22 +16,22 @@ namespace BackTestingPlatform.Tests
 
         public static void SaveAllTradeDays(int year1,int year2)
         {
-            DateTime date1 = new DateTime(year1, 1, 1);
-            DateTime date2 = new DateTime(year2+1, 1, 1);
-            TradeDaysInfoRepository repo = Platforms.container.Resolve<TradeDaysInfoRepositoryFromWind>();
-            var d = repo.fetch(date1,date2);
+            //DateTime date1 = new DateTime(year1, 1, 1);
+            //DateTime date2 = new DateTime(year2+1, 1, 1);
+            //TradeDaysInfoRepository repo = Platforms.container.Resolve<TradeDaysInfoRepositoryFromWind>();
+            //var d = repo.fetch(date1,date2);
 
-            Console.WriteLine("fetch {0} trade days from Wind.",d.Count());
-            var filePath = String.Format("trade_days_{0}_{1}.txt",year1,year2);
-            using (var sw = new StreamWriter(filePath))
-            {
-                for (int i = 0; i < d.Count; i++)
-                {
-                    sw.WriteLine(d[i].ToString("yyyyMMdd"));
-                }
-            }
+            //Console.WriteLine("fetch {0} trade days from Wind.",d.Count());
+            //var filePath = String.Format("trade_days_{0}_{1}.txt",year1,year2);
+            //using (var sw = new StreamWriter(filePath))
+            //{
+            //    for (int i = 0; i < d.Count; i++)
+            //    {
+            //        sw.WriteLine(d[i].ToString("yyyyMMdd"));
+            //    }
+            //}
 
-            Console.WriteLine("file ({0}) saved.", filePath);
+            //Console.WriteLine("file ({0}) saved.", filePath);
         }
 
         public static void readFile()
