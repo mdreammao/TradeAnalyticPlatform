e155dd42d3e6ef4cdf31a6d8b3bff1657302df22
heng mao <184772181@qq.com>
2016-08-25 09:29:25 +0800

    更新

--- BackTestingPlatform/BackTestingPlatform/DataAccess/Option/OptionDailyRepository.cs
+++ BackTestingPlatform/BackTestingPlatform/DataAccess/Option/OptionDailyRepository.cs
@@ -15,12 +15,43 @@ using BackTestingPlatform.DataAccess.Common;
 namespace BackTestingPlatform.DataAccess.Option
 {
 
-    public class OptionDailyRepository : BasicDataRepository<OptionDaily>
+    public class OptionDailyRepository : SequentialByYearRepository<OptionDaily>
     {
-        protected override List<OptionDaily> readFromWind()
+        protected override List<OptionDaily> readFromDefaultMssql(string code, DateTime dateStart, DateTime dateEnd, string tag = null, IDictionary<string, object> options = null)
         {
             throw new NotImplementedException();
         }
+
+        protected override List<OptionDaily> readFromWind(string code, DateTime dateStart, DateTime dateEnd, string tag = null, IDictionary<string, object> options = null)
+        {
+            WindAPI w = Platforms.GetWindAPI();
+            WindData wd = w.wsd(code, "open,high,low,close,volume,amt", dateStart, dateEnd, "Fill=Previous");
+            int len = wd.timeList.Length;
+            int fieldLen = wd.fieldList.Length;
+
+            var items = new List<OptionDaily>(len * fieldLen);
+            if (wd.data is double[])
+            {
+                double[] dataList = (double[])wd.data;
+                DateTime[] timeList = wd.timeList;
+                for (int k = 0; k < len; k++)
+                {
+                    items.Add(new OptionDaily
+                    {
+                        time = timeList[k],
+                        open = (double)dataList[k * fieldLen + 0],
+                        high = (double)dataList[k * fieldLen + 1],
+                        low = (double)dataList[k * fieldLen + 2],
+                        close = (double)dataList[k * fieldLen + 3],
+                        volume = (double)dataList[k * fieldLen + 4],
+                        amount = (double)dataList[k * fieldLen + 5]
+                    });
+                }
+            }
+
+            return items;
+        }
+    }
     }
 
 
