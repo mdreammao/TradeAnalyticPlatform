2214ed5e80c19760925a4a9b4f6b7d2a0ea941d9
heng mao <184772181@qq.com>
2016-09-01 10:40:09 +0800

    更新timeUtility

--- BackTestingPlatform/BackTestingPlatform/Utilities/TimeList/CreateTimeList.cs
+++ BackTestingPlatform/BackTestingPlatform/Utilities/TimeList/CreateTimeList.cs
@@ -41,6 +41,71 @@ namespace BackTestingPlatform.Utilities.TimeList
             }
             return time;
         }
+        public static List<int> GenerateMinuteIntList(params int[] timeList)
+        {
+            List<int> time = new List<int>();
+            if (timeList == null || timeList.Count() == 0 || timeList.Count() % 2 == 1)
+            {
+                log.Error("Wrong timeList input!");
+                return null;
+            }
+            for (int i = 0; i < timeList.Count(); i = i + 2)
+            {
+                time.AddRange(GenerateEachMinuteIntList(timeList[i], timeList[i + 1]));
+            }
+            return time;
+        }
+
+        private static List<int> GenerateEachMinuteIntList(int start,int end)
+        {
+            List<int> time = new List<int>();
+            if (end <= start)
+            {
+                log.Error("Wrong timeList input!");
+                return null;
+            }
+            DateTime now = Kit.ToDateTime(19871213, start);
+            DateTime endTime = Kit.ToDateTime(19871213, end);
+            while (now <= endTime)
+            {
+                time.Add(Convert.ToInt32(now.ToString("HHmmss")));
+                now = now.AddMinutes(1);
+            }
+            return time;
+        }
+
+        public static List<int> GenerateTickIntList(params int[] timeList)
+        {
+            List<int> time = new List<int>();
+            if (timeList == null || timeList.Count() == 0 || timeList.Count() % 2 == 1)
+            {
+                log.Error("Wrong timeList input!");
+                return null;
+            }
+            for (int i = 0; i < timeList.Count(); i = i + 2)
+            {
+                time.AddRange(GenerateEachTickIntList(timeList[i], timeList[i + 1]));
+            }
+            return time;
+        }
+
+        private static List<int> GenerateEachTickIntList(int start, int end)
+        {
+            List<int> time = new List<int>();
+            if (end <= start)
+            {
+                log.Error("Wrong timeList input!");
+                return null;
+            }
+            DateTime now = Kit.ToDateTime(19871213, start);
+            DateTime endTime = Kit.ToDateTime(19871213, end);
+            while (now <= endTime)
+            {
+                time.Add(Convert.ToInt32(now.ToString("HHmmssfff")));
+                now = now.AddMilliseconds(500);
+            }
+            return time;
+        }
         public static List<DateTime> GenerateMinuteTimeList(params DateTime[] timeList)
         {
             List<DateTime> time = new List<DateTime>();
