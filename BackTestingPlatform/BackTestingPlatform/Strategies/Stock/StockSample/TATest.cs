df245e1a89ef280d830ffb794b3e80cdf953b6dd
0FlynnHe0 <hfy89757@qq.com>
2016-10-12 11:20:10 +0800

    TAlib测试

---
+++ BackTestingPlatform/BackTestingPlatform/Strategies/Stock/StockSample/TATest.cs
@@ -0,0 +1,74 @@
+﻿using Autofac;
+using BackTestingPlatform.Core;
+using BackTestingPlatform.DataAccess;
+using BackTestingPlatform.DataAccess.Futures;
+using BackTestingPlatform.DataAccess.Option;
+using BackTestingPlatform.DataAccess.Stock;
+using BackTestingPlatform.Model.Common;
+using BackTestingPlatform.Model.Option;
+using BackTestingPlatform.Model.Positions;
+using BackTestingPlatform.Model.Signal;
+using BackTestingPlatform.Model.Stock;
+using BackTestingPlatform.Transaction;
+using BackTestingPlatform.Transaction.TransactionWithSlip;
+using BackTestingPlatform.Utilities;
+using BackTestingPlatform.Utilities.Option;
+using BackTestingPlatform.Utilities.TimeList;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BackTestingPlatform.Strategies.Stock.StockSample;
+using BackTestingPlatform.Strategies.Stock.StockSample01;
+using BackTestingPlatform.Utilities.Common;
+using TicTacTec.TA.Library;
+
+
+
+namespace BackTestingPlatform.Strategies.Stock.StockSample
+{
+    public class TATest
+    {
+        //策略参数
+        public const int periods = 5;
+
+        static Logger log = LogManager.GetCurrentClassLogger();
+        public  DateTime startDate, endDate;
+        public TATest(int start, int end)
+        {
+            startDate = Kit.ToDate(start);
+            endDate = Kit.ToDate(end);
+        }
+        string targetVariety = "510050.SH";
+
+        public void compute()
+        {
+            ///数据准备
+            //交易日信息
+            List<DateTime> tradeDays = DateUtils.GetTradeDays(startDate, endDate);
+            //50etf分钟数据准备，取全回测期的数据存放于data
+            Dictionary<string, List<KLine>> data = new Dictionary<string, List<KLine>>();
+            foreach (var tempDay in tradeDays)
+            {
+                var ETFData = Platforms.container.Resolve<StockMinuteRepository>().fetchFromLocalCsvOrWindAndSave(targetVariety, tempDay);
+                if (!data.ContainsKey(targetVariety))
+                    data.Add(targetVariety, ETFData.Cast<KLine>().ToList());
+                else
+                    data[targetVariety].AddRange(ETFData.Cast<KLine>().ToList());
+            }
+
+            ///指标计算
+            var closePrice = data[targetVariety].Select(x => x.close).ToArray();
+
+            TicTacTec.TA.Library.Core.RetCode retcode = new TicTacTec.TA.Library.Core.RetCode();
+            
+
+        }
+
+
+    }
+}
