c52431df0e8d21566753a5c79387abbb0139e4a5
heng mao <184772181@qq.com>
2016-10-25 11:12:57 +0800

    更新策略

---
+++ BackTestingPlatform/BackTestingPlatform/Strategies/Option/MaoHeng/CoveredOptionStrategy.cs
@@ -0,0 +1,44 @@
+﻿using Autofac;
+using BackTestingPlatform.Core;
+using BackTestingPlatform.DataAccess;
+using BackTestingPlatform.DataAccess.Futures;
+using BackTestingPlatform.DataAccess.Option;
+using BackTestingPlatform.DataAccess.Stock;
+using BackTestingPlatform.Model.Common;
+using BackTestingPlatform.Model.Option;
+using BackTestingPlatform.Model.Positions;
+using BackTestingPlatform.Model.Signal;
+using BackTestingPlatform.Model.Stock;
+using BackTestingPlatform.Transaction;
+using BackTestingPlatform.Transaction.MinuteTransactionWithSlip;
+using BackTestingPlatform.Utilities;
+using BackTestingPlatform.Utilities.Option;
+using BackTestingPlatform.Utilities.TimeList;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackTestingPlatform.Strategies.Option.MaoHeng
+{
+    public class CoveredOptionStrategy
+    {
+        static Logger log = LogManager.GetCurrentClassLogger();
+        private DateTime startdate, endDate;
+        public CoveredOptionStrategy(int start, int end)
+        {
+            startdate = Kit.ToDate(start);
+            endDate = Kit.ToDate(end);
+        }
+        public void compute()
+        {
+            log.Info("开始回测(回测期{0}到{1})", Kit.ToInt_yyyyMMdd(startdate), Kit.ToInt_yyyyMMdd(endDate));
+            var repo = Platforms.container.Resolve<OptionInfoRepository>();
+            var optionInfoList = repo.fetchFromLocalCsvOrWindAndSaveAndCache(1);
+            Caches.put("OptionInfo", optionInfoList);
+            List<DateTime> tradeDays = DateUtils.GetTradeDays(startdate, endDate);
+        }
+    }
+}
