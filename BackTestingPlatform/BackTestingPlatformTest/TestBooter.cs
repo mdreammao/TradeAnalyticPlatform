171ac58650bcb4d14e26baf1dca1a3cb641de2c2
Junming.Zhang <longmumin@gmail.com>
2016-10-25 09:17:59 +0800

    PLChart加入string 数组，作为X轴标签

--- BackTestingPlatform/BackTestingPlatformTest/TestBooter.cs
+++ BackTestingPlatform/BackTestingPlatformTest/TestBooter.cs
@@ -38,20 +38,28 @@ namespace BackTestingPlatform.Test
             //    //for (int j = 0; j < 44444; j++) x1 += Math.Sin(i);
             //           // Console.Write(i);
             //}
-            //测试图形化
-            //int startTime = 20160108;
-            //int endTime = 20160810;
-            //string secCode = "510050.SH";
 
+            
             //测试PLChart
             Dictionary<string, double[]> line = new Dictionary<string, double[]>();
             double[] y1 = { 120, 125, 100, 40, 105, 75 };
             double[] y2 = { 100, 66, 77, 40, 198, 20 };
             line.Add("text1", y1);
             line.Add("text2", y2);
+            string[] date = { "2012", "2013" };
             //最好将数据初始化以后，传入图形化类，但是需要考虑传递大数量级形参会不会出什么问题，考虑是传递参数还是数据
             //最后一个数据5表示k线为daily
-            Application.Run(new PLChart(line));
+            Application.Run(new PLChart(line, date));
+            
+
+            /*
+            //测试图形化
+            int startTime = 20160108;
+            int endTime = 20160810;
+            string secCode = "510050.SH";
+            Application.Run(new CandleStick(startTime, endTime, secCode, 5));
+            */
+
             Platforms.ShutDown();
             Console.ReadKey();
         }
