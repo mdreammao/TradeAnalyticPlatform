fef7c168cd7c8f049c59a9532f2f447818e47288
heng mao <184772181@qq.com>
2016-10-26 11:11:45 +0800

    更新策略

--- BackTestingPlatform/BackTestingPlatform/Strategies/Option/MaoHeng/CoveredOptionStrategy.cs
+++ BackTestingPlatform/BackTestingPlatform/Strategies/Option/MaoHeng/CoveredOptionStrategy.cs
@@ -9,6 +9,7 @@ using BackTestingPlatform.Model.Option;
 using BackTestingPlatform.Model.Positions;
 using BackTestingPlatform.Model.Signal;
 using BackTestingPlatform.Model.Stock;
+using BackTestingPlatform.Model.TALibrary;
 using BackTestingPlatform.Transaction;
 using BackTestingPlatform.Transaction.MinuteTransactionWithSlip;
 using BackTestingPlatform.Utilities;
@@ -26,19 +27,62 @@ namespace BackTestingPlatform.Strategies.Option.MaoHeng
     public class CoveredOptionStrategy
     {
         static Logger log = LogManager.GetCurrentClassLogger();
-        private DateTime startdate, endDate;
+        private DateTime startDate, endDate;
+        //回测参数设置
+        private double initialCapital = 10000000;
+        private double slipPoint = 0.000;
+        private string targetVariety = "510050.SH";
         public CoveredOptionStrategy(int start, int end)
         {
-            startdate = Kit.ToDate(start);
+            startDate = Kit.ToDate(start);
             endDate = Kit.ToDate(end);
         }
         public void compute()
         {
-            log.Info("开始回测(回测期{0}到{1})", Kit.ToInt_yyyyMMdd(startdate), Kit.ToInt_yyyyMMdd(endDate));
+            log.Info("开始回测(回测期{0}到{1})", Kit.ToInt_yyyyMMdd(startDate), Kit.ToInt_yyyyMMdd(endDate));
             var repo = Platforms.container.Resolve<OptionInfoRepository>();
             var optionInfoList = repo.fetchFromLocalCsvOrWindAndSaveAndCache(1);
             Caches.put("OptionInfo", optionInfoList);
-            List<DateTime> tradeDays = DateUtils.GetTradeDays(startdate, endDate);
-        }
+            
+            SortedDictionary<DateTime, Dictionary<string, PositionsWithDetail>> positions = new SortedDictionary<DateTime, Dictionary<string, PositionsWithDetail>>();
+            //初始化Account信息
+            BasicAccount myAccount = new BasicAccount();
+            myAccount.totalAssets = initialCapital;
+            myAccount.freeCash = myAccount.totalAssets;
+            //记录历史账户信息
+            List<BasicAccount> accountHistory = new List<BasicAccount>();
+
+            ///数据准备
+            //交易日信息
+            List<DateTime> tradeDays = DateUtils.GetTradeDays(startDate, endDate);
+            //50ETF的日线数据准备，从回测期开始之前100个交易开始取
+            List<StockDaily> dailyData = new List<StockDaily>();
+            dailyData = Platforms.container.Resolve<StockDailyRepository>().fetchFromLocalCsvOrWindAndSave(targetVariety, DateUtils.PreviousTradeDay(startDate,100), endDate);
+            //计算50ETF的EMA
+            var closePrice = dailyData.Select(x => x.close).ToArray();
+            var ema7 = TA_MA.EMA(closePrice, 7).ToArray();
+            var ema50 = TA_MA.EMA(closePrice, 50).ToArray();
+            //50etf分钟数据准备，取全回测期的数据存放于data
+            Dictionary<string, List<KLine>> data = new Dictionary<string, List<KLine>>();
+            foreach (var tempDay in tradeDays)
+            {
+                var ETFData = Platforms.container.Resolve<StockMinuteRepository>().fetchFromLocalCsvOrWindAndSave(targetVariety, tempDay);
+                if (!data.ContainsKey(targetVariety))
+                    data.Add(targetVariety, ETFData.Cast<KLine>().ToList());
+                else
+                    data[targetVariety].AddRange(ETFData.Cast<KLine>().ToList());
+            }
+            foreach (var day in tradeDays)
+            {
+
+                //取出当天的数据
+                Dictionary<string, List<KLine>> dataToday = new Dictionary<string, List<KLine>>();
+                foreach (var variety in data)
+                {
+                    dataToday.Add(variety.Key, data[variety.Key].FindAll(s => s.time.Year == day.Year && s.time.Month == day.Month && s.time.Day == day.Day));
+                }
+            }
+
+         }
     }
 }
