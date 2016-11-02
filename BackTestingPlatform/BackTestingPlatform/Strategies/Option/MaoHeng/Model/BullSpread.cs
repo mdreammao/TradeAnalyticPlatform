2b80881677a0b90a7c3db380780b142c982de637
heng mao <184772181@qq.com>
2016-11-02 13:59:47 +0800

    添加

---
+++ BackTestingPlatform/BackTestingPlatform/Strategies/Option/MaoHeng/Model/BullSpread.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackTestingPlatform.Strategies.Option.MaoHeng.Model
+{
+    public class BullSpread
+    {
+        public string code1, code2;
+        public double strike1, strike2;
+        public DateTime endDate;
+    }
+}
