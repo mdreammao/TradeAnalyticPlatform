accca26d0fa416ef1762531359b32dc4ec846268
heng mao <184772181@qq.com>
2016-11-07 09:01:25 +0800

    更新

---
+++ BackTestingPlatform/BackTestingPlatform/Utilities/SaveResult/ResultPathUtil.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackTestingPlatform.Utilities.SaveResult
+{
+    public  class ResultPathUtil
+    {
+        public static string GetLocalPath(string fullPath,string tag,string dateStr,string type,string parameters)
+        {
+            return fullPath.Replace("{tag}", tag).Replace("{date}", dateStr).Replace("{parameters}",parameters).Replace("{type}", "account");
+        }
+    }
+}
