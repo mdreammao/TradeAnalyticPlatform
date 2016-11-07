966ed8c1de80513212b0a170c89e94335517e2d4
heng mao <184772181@qq.com>
2016-11-07 11:00:34 +0800

    更新

---
+++ BackTestingPlatform/BackTestingPlatform/Utilities/SaveResult/Common/RecordUtil.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace BackTestingPlatform.Utilities.SaveResult.Common
+{
+    public class RecordUtil
+    {
+        public static void recordToCsv<T>(IList<T> data, string tag,string type, string parameters = "", string performance = "")
+        {
+            var fullPath = ConfigurationManager.AppSettings["CacheData.ResultPath"] + ConfigurationManager.AppSettings["CacheData.StrategyPath"];
+            var dateStr = Kit.ToInt_yyyyMMdd(DateTime.Now).ToString();
+            fullPath = ResultPathUtil.GetLocalPath(fullPath, tag, dateStr, type, parameters, performance);
+            var dt = DataTableUtils.ToDataTable(data);
+            CsvFileUtils.WriteToCsvFile(fullPath, dt);
+        }
+    }
+}
---
+++ BackTestingPlatform/BackTestingPlatform/Utilities/SaveResult/Option/OptionRecordUtil.cs
@@ -0,0 +1,40 @@
+﻿using BackTestingPlatform.Core;
+using BackTestingPlatform.Model.Common;
+using BackTestingPlatform.Model.Option;
+using BackTestingPlatform.Model.Positions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackTestingPlatform.Utilities.SaveResult.Option
+{
+    public class OptionRecordUtil
+    {
+        public static List<HoldingStatus> Transfer(SortedDictionary<DateTime, Dictionary<string,PositionsWithDetail>> positions)
+        {
+            List<OptionInfo> optionInfo = (List<OptionInfo>)Caches.get("OptionInfo");
+            List<HoldingStatus> myRecord = new List<HoldingStatus>();
+            var positionShot =positions.Last().Value;
+            foreach (var item in positionShot)
+            {
+                string remarks = "";
+                foreach (var option in optionInfo)
+                {
+                    if (option.optionCode==item.Key)
+                    {
+                        remarks += option.optionName;
+                    }
+                }
+                foreach (var records in item.Value.record)
+                {
+                    
+                    myRecord.Add(new HoldingStatus(records.time, records.code, records.price, records.volume,remarks));
+                }
+            }
+            myRecord = myRecord.OrderBy(x => x.time).ToList();
+            return myRecord;
+        }
+    }
+}
