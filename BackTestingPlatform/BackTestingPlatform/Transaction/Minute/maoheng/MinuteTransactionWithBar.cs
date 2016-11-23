bc0dbb98b59bdabcbe5cff41f06847a6d80a57d2
heng mao <184772181@qq.com>
2016-11-23 14:24:56 +0800

    添加成交判断

---
+++ BackTestingPlatform/BackTestingPlatform/Transaction/Minute/maoheng/MinuteTransactionWithBar.cs
@@ -0,0 +1,21 @@
+﻿using BackTestingPlatform.Model.Common;
+using BackTestingPlatform.Model.Positions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackTestingPlatform.Transaction.Minute.maoheng
+{
+    /// <summary>
+    /// 根据分钟K线图来判断成交。具体的，如果开多仓，开仓价格必须大于等于K线中的最低价格，如果开空仓，开仓价格必须小于等于K线中的最高价格。
+    /// </summary>
+    public class MinuteTransactionWithBar
+    {
+        public static DateTime ComputePosition(Dictionary<string, List<KLine>> data, ref SortedDictionary<DateTime, Dictionary<string, PositionsWithDetail>> positions, ref BasicAccount myAccount, DateTime now)
+        {
+            return now.AddMinutes(1);
+        }
+    }
+}
