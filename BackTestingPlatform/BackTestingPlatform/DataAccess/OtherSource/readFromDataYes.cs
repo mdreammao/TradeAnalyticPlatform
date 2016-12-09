25ee9f29e4d585c80eea70152cf55df3a4081d9f
heng mao <mdream05301@163.com>
2016-12-09 10:36:18 +0800

    更新数据的读取

--- BackTestingPlatform/BackTestingPlatform/DataAccess/OtherSource/readFromDataYes.cs
+++ BackTestingPlatform/BackTestingPlatform/DataAccess/OtherSource/readFromDataYes.cs
@@ -1,8 +1,12 @@
 ﻿using BackTestingPlatform.Model.Common;
 using BackTestingPlatform.Utilities;
+using NLog;
+using NLog.Fluent;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,23 +15,68 @@ namespace BackTestingPlatform.DataAccess.OtherSource
 {
     public static class readFromDataYes
     {
+        static Logger log = LogManager.GetCurrentClassLogger();
         public static void getData(string path,string fileName)
         {
-            Dictionary<string, Dictionary<int, List<KLine>>> dic = new Dictionary<string, Dictionary<int, List<KLine>>>();
-            string header = "time,code,open,close,high,low,volume,turnover,totalvolume,totalturnover,requesttime";
-            DataTable dt = CsvFileUtils.ReadFromCsvFile(path,false,header);
-            foreach (DataRow row in dt.Rows)
+            Dictionary<string, Dictionary<string, List<KLine>>> dic = new Dictionary<string, Dictionary<string, List<KLine>>>();
+            //string header = "time,code,open,close,high,low,volume,turnover,totalvolume,totalturnover,requesttime";
+            try
             {
-                var time = row["time"];
-                string code =Convert.ToString(row["code"]);
-                double open = Convert.ToDouble(row["open"]);
-                double close = Convert.ToDouble(row["close"]);
-                double high = Convert.ToDouble(row["high"]);
-                double low = Convert.ToDouble(row["low"]);
-                double volume = Convert.ToDouble(row["volume"]);
-                double turnover = Convert.ToDouble(row["turnover"]);
+                using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+                {
+                    using (StreamReader sr = new StreamReader(fs, Encoding.UTF8))
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            string[] row = sr.ReadLine().Split(',').Select(toNonDoubleQuotedString).ToArray();
+                            DateTime time =DateTime.ParseExact(Convert.ToString(row[0]),"yyyyMMddHHmm",null);
+                            string today = time.ToString("yyyyMMdd");
+                            string code = Convert.ToString(row[1]).ToUpper();
+                            double open = Convert.ToDouble(row[2])/10000.0;
+                            double close = Convert.ToDouble(row[3])/10000.0;
+                            double high = Convert.ToDouble(row[4])/10000.0;
+                            double low = Convert.ToDouble(row[5])/10000.0;
+                            double volume = Convert.ToDouble(row[6]);
+                            double turnover = Convert.ToDouble(row[7]);
+                            List<KLine> data0 = new List<KLine>();
+                            if (dic.ContainsKey(code)==true)
+                            {
+                                if (dic[code].ContainsKey(today)==true)
+                                {
+                                    data0 = dic[code][today];
+                                }
+                                else
+                                {
+                                    dic[code].Add(today, new List<KLine>());
+                                }
+                            }
+                            else
+                            {
+                                dic.Add(code, new Dictionary<string, List<KLine>>());
+                                dic[code].Add(today, new List<KLine>());
+                            }
+                            data0.Add(new KLine { time = time, open = open, close = close, high = high, low = low, volume = volume, amount = turnover });
+                            dic[code][today] = data0;
+                        }
+                    }
+                }
             }
-
+            catch (Exception e)
+            {
+                log.Error(e);
+            }
+        }
+        static string toDoubleQuotedString(string src)
+        {
+            return string.Concat("\"", src.Replace("\"", "\"\""), "\"");
+        }
+        static string toNonDoubleQuotedString(string src)
+        {
+            int len = src.Length;
+            if (src[0] == '\"' && src[len - 1] == '\"')
+                return src.Substring(1, len - 2);
+            else
+                return src;
         }
     }
 }
