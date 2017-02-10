a77dad20ac5ac7ac324f94a15d94ba31d548c4c4
heng mao <184772181@qq.com>
2017-02-10 15:33:22 +0800

    更新

--- BackTestingPlatform/BackTestingPlatform/Utilities/Spline/MatrixInverse.cs
+++ BackTestingPlatform/BackTestingPlatform/Utilities/Spline/MatrixInverse.cs
@@ -5,7 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using MathNet.Numerics.LinearAlgebra;
 using MathNet.Numerics.LinearAlgebra.Double;
-
+//我曾做过测试，将double[,] 先转成Math矩阵，然后进行矩阵运算，再利用matrix2.ToArray()将Math矩阵转换成double[,]，其运算时间和直接利用C#编写的矩阵运算相差很小。
+// 但如果是利用for循环将double数组的数值赋值给Math矩阵进行矩阵运算，然后再利用for循环将Math矩阵赋值给某个double[，] 数组，其运算时间可以减少1/3。在开发效率和运算效率上，使用的时候可以根据需要进行取舍。
 namespace BackTestingPlatform.Utilities.Spline
 {
     public static class MatrixInverse
